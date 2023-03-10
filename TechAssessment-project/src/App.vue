b8f910c507c36e71570f98212a69cd8c0c50a73e
Clara Aw <clara.aw.jq@gmail.com>
2023-03-11 00:10:00 +0800

    add time and date input

--- TechAssessment-project/src/App.vue
+++ TechAssessment-project/src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+import Form from './components/Form.vue'
 </script>
 
 <template>
@@ -8,7 +9,8 @@ import TheWelcome from './components/TheWelcome.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <!-- <HelloWorld msg="hello" /> -->
+      <Form></Form>
     </div>
   </header>
 
--- TechAssessment-project/src/components/Form.vue
+++ TechAssessment-project/src/components/Form.vue
@@ -2,10 +2,66 @@
 </script>
 
 <template>
-    <div>
-        <p>Please select a date</p>
-        <select>
-        </select>
+    <div class="form">
+
+        <div>
+            <h2>Please select a date:</h2>
+            <input type="date" id="date">
+        </div>
+
+        <br>
+
+        <div id="timeInput">
+            <h2>Please select a time:</h2>
+
+            <label for="" class="time">HOUR</label>
+            <select name="" id="">
+                <option value="" v-for="hr in 24">
+                    {{ hr }}
+                </option>
+            </select>
+
+            <label for="" class="time">MINUTE</label>
+            <select name="" id="">
+                <option value="" v-for="(i, min) in 60"> 
+                    {{ min }}
+                </option>
+            </select>
+
+            <label for="" class="time">SECOND</label>
+            <select name="" id="">
+                <option value="" v-for="(i, sec) in 60"> 
+                    {{ sec }}
+                </option>
+            </select>
+
+        </div>
+
 
     </div>
 </template>
+
+<style scoped>
+
+.form{
+    margin: auto;
+    /* border: 1px solid white; */
+    text-align: center;
+}
+
+.time{
+    padding-left: 5px;
+    padding-right: 5px;
+    font-size: large;
+}
+
+#date{
+    font-size: large;
+    border-radius: 9px;
+}
+
+#timeInput{
+    flex: auto;
+}
+
+</style>
