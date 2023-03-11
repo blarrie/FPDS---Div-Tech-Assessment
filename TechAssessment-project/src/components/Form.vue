9edbddf834aa4e388fcc051d6087ad7052e23848
Clara Aw <clara.aw.jq@gmail.com>
2023-03-11 23:51:29 +0800

    changed submit button design

--- TechAssessment-project/src/components/Form.vue
+++ TechAssessment-project/src/components/Form.vue
@@ -1,72 +1,83 @@
 <script>
 export default {
+    name: "Form",
+    data() {
+        return {
+            date: "",
+            hour: "",
+            min: "",
+            sec: "",
+        };
+    },
+    methods: {
+        postData(e) {
+            console.log(this.date);
+            console.log(this.hour);
+            console.log(this.min);
+            console.log(this.sec);
 
-}
+            e.preventDefault();
+        },
+    },
+};
 </script>
 
 <template>
     <div class="form">
+        <form @submit="postData" method="post">
+            <div>
+                <p class="h3">Please select a date:</p>
+                <input type="date" id="date" name="date" v-model="date" />
+            </div>
 
-        <div>
-            <p class="h3">Please select a date:</p>
-            <input type="date" id="date">
-        </div>
+            <br/>
 
-        <br>
+            <div >
+            
+                <p class="h3">Please select a time:</p>
+                    <label for="" class="time">HOUR</label>
+                    <select class="form-select" name="hour" id="hour" v-model="hour">
+                        <option value="" v-for="hr in 24">
+                            {{ hr }}
+                        </option>
+                    </select>
 
-        <div>
-            <label for="time-input"></label>
-            <p class="h3">Please select a time:</p>
+                    <label for="minute" class="time">MINUTE</label>
+                    <select class="form-select" name="min" id="minute" v-model="min">
+                        <option :value="{ min }" v-for="(i, min) in 60">
+                            {{ min }}
+                        </option>
+                    </select>
 
-            <label for="" class="time">HOUR</label>
-            <select name="" id="">
-                <option value="" v-for="hr in 24">
-                    {{ hr }}
-                </option>
-            </select>
+                    <label for="second" class="time">SECOND</label>
+                    <select class="form-select" name="sec" id="second" v-model="sec">
+                        <option value="" v-for="(i, sec) in 60">
+                            {{ sec }}
+                        </option>
+                    </select>
+            </div>
 
-            <label for="minute" class="time">MINUTE</label>
-
-            <select name="" id="minute">
-                <option value="" v-for="(i, min) in 60"> 
-                    {{ min }}
-                </option>
-            </select>
-
-            <label for="second" class="time">SECOND</label>
-            <select name="" id="second">
-                <option value="" v-for="(i, sec) in 60"> 
-                    {{ sec }}
-                </option>
-            </select>
-
-
-
-        </div>
-
-
+            <button class="btn btn-primary m-3" type="submit">Submit</button>
+        </form>
     </div>
 </template>
 
 <style scoped>
-
-.form{
+.form {
     margin: auto;
     /* border: 1px solid white; */
     text-align: center;
     font-family: Arial, Helvetica, sans-serif;
 }
 
-.time{
+.time {
     padding-left: 5px;
     padding-right: 5px;
     font-size: large;
 }
 
-#date{
+#date {
     font-size: large;
     border-radius: 9px;
 }
-
-
 </style>
