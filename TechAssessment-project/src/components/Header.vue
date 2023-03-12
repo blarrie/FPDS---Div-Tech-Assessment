7822ba7452e80a91569639139b6ed16711986125
Clara Aw <clara.aw.jq@gmail.com>
2023-03-12 22:15:17 +0800

    created header component

---
+++ TechAssessment-project/src/components/Header.vue
@@ -0,0 +1,18 @@
+<script>
+export default{
+    name: "Header",
+}
+</script>
+
+<template>
+    <nav class="navbar" style="background-color: antiquewhite;">
+        <div class="container-fluid m-auto">
+           
+            <span class="m-auto h3">
+                <img src="./icons/imgs/trafficIcon.png" alt="" style="width: 50px;">
+                Traffic Images and Weather
+            </span>
+
+        </div>
+    </nav>
+</template>
